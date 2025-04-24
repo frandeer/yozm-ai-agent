30c7471333c9061066ede8eb0aadd0ae7b3a2027
wapj <wapj2000@gmail.com>
2025-04-25 03:11:12 +0900

    [2.3] 어린왕자 페르소나를 가진 챗봇 만들기

--- chapter2/chat_bot2.py
+++ chapter2/chat_bot2.py
@@ -3,25 +3,25 @@ from openai import OpenAI
 client = OpenAI()
 
 
-def chatbot_response(user_message: str, response_id=None):
+def chatbot_response(user_message: str, previous_response_id=None):
     result = client.responses.create(
-        model="gpt-4.1-mini", input=user_message, previous_response_id=response_id
+        model="gpt-4.1-mini",instructions="", input=user_message, previous_response_id=previous_response_id
     )
     return result
 
 
 if __name__ == "__main__":
     # 여기서 사용자 메시지를 입력받고 응답을 출력합니다.
-    response_id = None
+    previous_response_id = None
     while True:
         user_message = input("메시지: ")
         if user_message.lower() == "exit":
             print("대화를 종료 합니다.")
             break
 
-        result = chatbot_response(user_message, response_id)
-        response_id = result.id
-        print(result.output_text)
+        result = chatbot_response(user_message, previous_response_id)
+        previous_response_id = result.id
+        print("챗봇 :", result.output_text)
 
 # 첫번째 예제에는 내 이름을 모른다.
 # 두번째 예제에는 내 이름을 알고 있다. response_id를 사용
