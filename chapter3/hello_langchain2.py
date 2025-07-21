16abcc36d814370aa9a07860660ab66359b22459
wapj <wapj2000@gmail.com>
2025-07-21 19:00:40 +0900

    [3장] 예제 수정

--- chapter3/hello_langchain2.py
+++ chapter3/hello_langchain2.py
@@ -1,5 +1,5 @@
 from langchain.chat_models import init_chat_model
 
-model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
+model = init_chat_model("claude-sonnet-4-20250514", model_provider="anthropic")
 result = model.invoke("랭체인이 뭔가요?")
 print(result.content)
--- chapter3/langchain_chat_model.py
+++ chapter3/langchain_chat_model.py
@@ -5,7 +5,7 @@ if random.random() < 0.5:
     print("gpt-4.1-mini selected")
     model = init_chat_model("gpt-4.1-mini")
 else:
-    print("claude-3-5-sonnet-latest selected")
-    model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
+    print("claude-sonnet-4-20250514 selected")
+    model = init_chat_model("claude-sonnet-4-20250514", model_provider="anthropic")
 result = model.invoke("RAG가 뭔가요?")
 print(result.content)
